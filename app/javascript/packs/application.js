7fb3114f9400c5a2f45b96c4501397bb8ecd4b6b
“theduc151” <nguyentheduc15196@gmail.com>
2022-03-25 15:22:44 +0700

    Implement basic login

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+import "bootstrap"
+import "jquery"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User <  ActiveRecord::Base
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,4 +8,10 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
